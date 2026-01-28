1d8393b76c558e2e083ab3f83e7e4796668fff81
cryolins <hepeilin07@gmail.com>
2026-01-28 00:39:46 -0500

    redirect patch via env variable

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // getting secret
 const SECRET = process.env.NEXTAUTH_SECRET!;
+const BASE_URL = process.env.NEXT_BASE_URL!;
 
 if (!SECRET) {
     throw new Error("no next-auth secret found")
@@ -20,10 +21,10 @@ export default async function authMiddleware (req: NextRequest) {
 
     if (!token && protPaths.includes(nextPath)) {
         // trying to access protected paths without logging in -> redirect to login page
-        return NextResponse.redirect(new URL("/login", req.url));
+        return NextResponse.redirect(new URL("/login", BASE_URL));
     } else if (token && authPaths.includes(nextPath)) {
         // trying to access login paths while being logged in -> redirect to map page
-        return NextResponse.redirect(new URL("/map", req.url));
+        return NextResponse.redirect(new URL("/map", BASE_URL));
     }
     // can add rbac or other conditions as needed
     else{
